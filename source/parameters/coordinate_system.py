8016bdca1b0e0bbe2024c5e7a005f1a180b0ad04
Jay Chong <jaychong1203@gmail.com>
2024-03-08 15:58:08 -0800

    updated velocity and coordinate system

--- source/parameters/coordinate_system.py
+++ source/parameters/coordinate_system.py
@@ -1,8 +1,7 @@
 import numpy as np
 
-x_values = np.linspace(0, 64, 65)  
-y_values = np.linspace(0, 64, 65)  
-
-w_values = np.sqrt(np.add.outer(x_values**2, y_values**2))
-
-print(w_values)
+def coordinate_system():
+    x_values = np.linspace(0.1, 64, 65)
+    y_values = np.linspace(0.1, 64, 65)
+    R_values = np.sqrt(np.add.outer(x_values**2, y_values**2))
+    return R_values
--- source/parameters/wind_velocity.py
+++ source/parameters/wind_velocity.py
@@ -2,17 +2,36 @@
 # Abbott & Klein 1975), the poloidal component of the wind velocity
 # V_p parametrized as equation 5 on page 4 of Kurosawa et al. (2006)
 
-import math
-from coordinate_system import w_values 
+import numpy as np
+from coordinate_system import coordinate_system
 
 # some constants for now
 
-c_s = 10^5 # sound speed at the wind launching point in cm/s
+R = coordinate_system()
+c_s = 10**5 # sound speed at the wind launching point in cm/s
 f = 0.9 # constant scale factor of the asymptotic terminal
-v_esc = ((42*10^5) / math.sqrt(R)) # what is this?
 phi = 0.9 # what is this?
+beta = 0.2 # what is this?
+R_s = ... # what is this?
+l = ... # what is this?
 
-def calculate_wind_velocity(c_s, f, v_esc, phi):
-    v_p
+
+def calculate_escape_velocity(R):
+    R_safe = np.maximum(R, 1e-10)  
+    v_esc = (42 * 10**5) / np.sqrt(R_safe) 
+    return v_esc
+
+v_esc = calculate_escape_velocity(R)
+
+def calculate_wind_velocity(c_s, f, v_esc, phi, beta, R_s, l):
+    # denominator = np.maximum(l + R_s, 1e-10)  
+    # term = (1 - R_s / denominator) ** beta
+    # v_p = c_s + (f * v_esc - c_s) * term 
+    v_p = c_s + (f * v_esc - c_s) * beta
     return v_p
 
+v_p = calculate_wind_velocity(c_s, f, v_esc, phi, beta, R_s=None, l=None)
+
+
+print(v_p) 
+np
