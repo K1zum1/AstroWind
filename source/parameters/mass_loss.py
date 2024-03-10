c55d54e29d9ba2929f366321ac7890059d5ce374
Jay Chong <jaychong1203@gmail.com>
2024-03-10 10:57:30 -0700

    m_dot_w not defined

--- source/parameters/mass_loss.py
+++ source/parameters/mass_loss.py
@@ -4,7 +4,12 @@
 
 alpha = 0.76 # consistent with the collimated disc-wind model
 q =  -1.15 # dust radiative transfer model of the innermost part of the accetion disc
-p = 4*alpha * q # index of the mid-plane temperature 
+p = 4*alpha * q # index of the mid-plane temperature
 
-def calculate_mass_loss_rate(p):
+def calculate_mass_loss_rate():
+     
     return m_dot_w**p
+
+m_dot_wi = calculate_mass_loss_rate()
+
+# print(m_dot_wi)
