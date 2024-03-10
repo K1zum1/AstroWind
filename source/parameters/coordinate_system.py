012b61258b83fe0d586ccb7405c2f0ca64d8453c
Jay Chong <jaychong1203@gmail.com>
2024-03-10 11:04:27 -0700

    m_dot_w defined

--- source/parameters/coordinate_system.py
+++ source/parameters/coordinate_system.py
@@ -1,12 +1,11 @@
 import numpy as np
 
 
-x_values = np.linspace(0.1, 64, 65)
-y_values = np.linspace(0.1, 64, 65)
-
-def coordinate_system(x_values, y_values):
+def coordinate_system():
+    x_values = np.linspace(0.1, 64, 65)
+    y_values = np.linspace(0.1, 64, 65)
     R_values = np.sqrt(np.add.outer(x_values**2, y_values**2))
     return R_values
 
-R_values = coordinate_system(x_values, y_values)
+R_values = coordinate_system()
 # print(R_values)
--- source/parameters/mass_loss.py
+++ source/parameters/mass_loss.py
@@ -2,14 +2,17 @@
 # to the mid-plane temperature of the disc, and is a function
 # of the cylindrical radius. m_dot_w is parameterized as equation 2 on page 4 of Kurosawa et al. (2006)
 
+from coordinate_system import coordinate_system
+
 alpha = 0.76 # consistent with the collimated disc-wind model
 q =  -1.15 # dust radiative transfer model of the innermost part of the accetion disc
 p = 4*alpha * q # index of the mid-plane temperature
+R_values = coordinate_system()
 
-def calculate_mass_loss_rate():
+def calculate_mass_loss_rate(R_values):
      
-    return m_dot_w**p
+    return R_values**p
 
-m_dot_wi = calculate_mass_loss_rate()
+m_dot_wi = calculate_mass_loss_rate(R_values)
 
 # print(m_dot_wi)
