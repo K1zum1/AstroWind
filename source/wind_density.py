8d03bfa6762f9fd0a60b5f24aabd931ffce1953f
Jay Chong <jaychong1203@gmail.com>
2024-03-11 11:54:38 -0700

    function call

--- source/wind_density.py
+++ source/wind_density.py
@@ -13,3 +13,5 @@ D_wi_l = get_source_point()
 def wind_density(m_dot_wi, vp_wi_l, delta, d, D_wi_l):
     rho_wi_l = (m_dot_wi / (vp_wi_l * sp.Abs(sp.cos(delta)))) * (d / (D_wi_l * sp.cos(delta)))**2
     return rho_wi_l
+
+wind_density = wind_density(m_dot_wi, vp_wi_l, delta, d, D_wi_l)
