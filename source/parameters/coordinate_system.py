9a3df7c2176fa936ce2d627b017d6321b7a40727
Jay Chong <jaychong1203@gmail.com>
2024-03-12 15:48:07 -0700

    moved source point into coordinate module

--- source/parameters/coordinate_system.py
+++ source/parameters/coordinate_system.py
@@ -1,11 +1,17 @@
 import numpy as np
 
-
 def coordinate_system():
     x_values = np.linspace(0.1, 64, 65)
     y_values = np.linspace(0.1, 64, 65)
     R_values = np.sqrt(np.add.outer(x_values**2, y_values**2))
-    return R_values
+    return R_values, y_values 
 
-R_values = coordinate_system()
-# print(R_values)
+R_values, y_values = coordinate_system()
+
+def get_source_point(y_values):
+    d = -5
+    D = y_values + d
+    return D
+
+D = get_source_point(y_values)
+print(D)
