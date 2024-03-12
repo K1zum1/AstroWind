a4070aaeb2181f56a75093055fddc4f52a3e4930
Jay Chong <jaychong1203@gmail.com>
2024-03-12 15:47:35 -0700

    correctly unpacked return values from coordinate

--- source/parameters/mass_loss.py
+++ source/parameters/mass_loss.py
@@ -4,15 +4,16 @@
 
 from coordinate_system import coordinate_system
 
-alpha = 0.76 # consistent with the collimated disc-wind model
-q =  -1.15 # dust radiative transfer model of the innermost part of the accetion disc
-p = 4*alpha * q # index of the mid-plane temperature
-R_values = coordinate_system()
+
+R_values, _ = coordinate_system()
 
 def calculate_mass_loss_rate(R_values):
-     
-    return R_values**p
+    alpha = 0.76 # consistent with the collimated disc-wind model
+    q =  -1.15 # dust radiative transfer model of the innermost part of the accetion disc
+    p = 4*alpha * q # index of the mid-plane temperature 
+
+    return (R_values)**p
 
 m_dot_wi = calculate_mass_loss_rate(R_values)
 
-# print(m_dot_wi)
+#print(m_dot_wi)
