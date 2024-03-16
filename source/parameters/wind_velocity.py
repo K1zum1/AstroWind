75f100d1165699feafe0ce53da05ff7ae471cedb
Jay Chong <jaychong1203@gmail.com>
2024-03-16 12:15:20 -0700

    changed power scale and modified velocity formula

--- source/parameters/wind_velocity.py
+++ source/parameters/wind_velocity.py
@@ -1,9 +1,7 @@
 import numpy as np
 from coordinate_system import coordinate_system
 
-# some constants for now
 R_values, y_values = coordinate_system()
-
 
 def calculate_escape_velocity(R_values):
     R_safe = np.maximum(R_values, 1e-10)  
@@ -12,18 +10,10 @@ def calculate_escape_velocity(R_values):
 
 v_esc = calculate_escape_velocity(R_values)
 
-def calculate_wind_velocity():
+def calculate_wind_velocity(R_values):
     c_s = 10**5 # sound speed at the wind launching point in cm/s
     f = 0.9 # constant scale factor of the asymptotic terminal
     phi = 0.9 # what is this?
-    beta = 0.2 # what is this?
-    R_s = ... # what is this?
-    l = ... # what is this?
-    # denominator = np.maximum(l + R_s, 1e-10)  
-    # term = (1 - R_s / denominator) ** beta
-    # v_p = c_s + (f * v_esc - c_s) * term 
-    v_p = c_s + (f * v_esc - c_s) * beta
+    beta = 1 # power scale
+    v_p = c_s + (f * v_esc - c_s) * (R_values / (R_values + 1.5e14)) ** beta
     return v_p
-
-
-#print(v_p)
