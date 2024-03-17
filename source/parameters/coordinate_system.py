ae544b6f4b36cbe77a93fa872118f83012120477
Jay Chong <jaychong1203@gmail.com>
2024-03-17 14:33:51 -0700

    multipied the y_values by au

--- source/parameters/coordinate_system.py
+++ source/parameters/coordinate_system.py
@@ -1,18 +1,15 @@
 import numpy as np
 from astro_constants import AU
 
-
 def coordinate_system():
-    x_values = np.linspace(0.1, 64, 65)
-    y_values = np.linspace(0.1, 64, 65)
+    x_values = np.linspace(0.1, 64, 65) * AU
+    y_values = np.linspace(0.1, 64, 65) * AU
     R_values = np.sqrt(np.add.outer(x_values**2, y_values**2))
-    R_values *= AU
     return R_values, y_values 
 
 R_values, y_values = coordinate_system()
 
 def get_source_point(y_values):
-    d = -5*AU
+    d = -5 * AU
     D = y_values + d
     return D
-
