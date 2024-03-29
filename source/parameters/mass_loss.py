04bd01b430a7486ef48f3df956c8368dfb543d1b
Jay Chong <jaychong1203@gmail.com>
2024-03-29 12:51:39 -0700

    mass loss formula change

--- source/parameters/mass_loss.py
+++ source/parameters/mass_loss.py
@@ -1,15 +1,20 @@
 from coordinate_system import coordinate_system
+import numpy as np
+
+R_values, _, R_plane = coordinate_system()
+
+def calculate_mass_loss_rate(R_values, R_plane):
+    alpha = 0.76
+    q = -1.15
+    p = 4 * alpha * q
+    M_dot_w = 1e17
+    r_in = np.min(R_plane)
+    r_out = np.max(R_values)
+    k = ((p + 2) * M_dot_w) / (2 * np.pi * (r_out**(p + 2) - r_in**(p + 2)))
+    mass_loss_rate = k * (R_values)**p
+
+    return mass_loss_rate
 
 
-R_values, _ = coordinate_system()
+mass_loss_rate = calculate_mass_loss_rate(R_values, R_plane)
 
-def calculate_mass_loss_rate(R_values):
-    alpha = 0.76 # consistent with the collimated disc-wind model
-    q =  -1.15 # dust radiative transfer model of the innermost part of the accetion disc
-    p = 4*alpha * q # index of the mid-plane temperature 
-
-    return (R_values)**p
-
-
-
-#print(m_dot_wi)
