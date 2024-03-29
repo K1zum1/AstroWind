2a5f2c947285114b23f767ebeed34704eb57bff6
Jay Chong <jaychong1203@gmail.com>
2024-03-29 12:51:46 -0700

    r-plane

--- source/parameters/coordinate_system.py
+++ source/parameters/coordinate_system.py
@@ -2,14 +2,15 @@ import numpy as np
 from astro_constants import AU
 
 def coordinate_system():
-    x_values = np.linspace(0.1, 64, 64) * AU  # Adjusted to 64 points for a 64x64x64 grid
+    x_values = np.linspace(0.1, 64, 64) * AU  
     y_values = np.linspace(0.1, 64, 64) * AU
-    z_values = np.linspace(0.1, 64, 64) * AU  # Added z_values for the third dimension
+    z_values = np.linspace(0.1, 64, 64) * AU  
     X, Y, Z = np.meshgrid(x_values, y_values, z_values, indexing='ij')
+    R_plane = np.sqrt(X**2 + Z**2)
     R_values = np.sqrt(X**2 + Y**2 + Z**2)
-    return R_values, y_values
+    return R_values, y_values, R_plane
 
-R_values, y_values = coordinate_system()
+R_values, y_values, R_plane = coordinate_system()
 
 def get_source_point(y_values):
     d = -5 * AU
