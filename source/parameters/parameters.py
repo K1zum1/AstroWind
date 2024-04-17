c7f253e65d3fb9eaba8074580c72354897994203
Jay Chong <jaychong1203@gmail.com>
2024-04-17 14:54:11 -0700

    parameter documentation

--- source/parameters/parameters.py
+++ source/parameters/parameters.py
@@ -8,12 +8,12 @@ MU = 2.3e-24
 
 # parameters, can be changed to fit the model
 
-lmbda = 1.6
+lmbda = 1.6 # Alfven lever parameter
 
 d = -5 * AU
 
-p = 3.4959999999999996
+p = 3.4959999999999996 # Change in mass loss as a function of radius p
 
 GM_star = 1.1521e10
 
-M_dot_w = 1e17
+M_dot_w = 1e17 # Wind mass loss rate
