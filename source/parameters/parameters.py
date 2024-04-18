f9b7b4135714ba988de197c72c9839e8d5d9f9f9
Jay Chong <jaychong1203@gmail.com>
2024-04-18 12:04:44 -0700

    r_in r_out readin parameter

--- source/parameters/parameters.py
+++ source/parameters/parameters.py
@@ -1,19 +1,17 @@
-# constants
+# Astronomical Constants
+AU = 149597870  # Astronomical unit in kilometers
+MS = 1.99e+33   # Solar mass in grams
+MU = 2.3e-24    # Mean molecular weight in grams
 
-AU = 149597870
+# Star Parameters
+GM_star = 1.1521e10  # Gravitational parameter for the star
 
-MS = 1.99e+33
+# Wind Parameters
+M_dot_w = 1e17  # Wind mass loss rate
+lmbda = 1.6     # Alfven lever parameter
+d = -5 * AU     # Distance for wind source point calculation
+p = 3.4959999999999996  # Exponent in mass loss rate calculation
 
-MU = 2.3e-24    
-
-# parameters, can be changed to fit the model
-
-lmbda = 1.6 # Alfven lever parameter
-
-d = -5 * AU
-
-p = 3.4959999999999996 # Change in mass loss as a function of radius p
-
-GM_star = 1.1521e10
-
-M_dot_w = 1e17 # Wind mass loss rate
+# Radial Boundaries for Mass Loss Calculation
+r_in = 10 * AU  # Inner boundary radius
+r_out = 50 * AU  # Outer boundary radius
