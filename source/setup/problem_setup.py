271228e7a4e4e115854656bae7017c6260a96f16
Jay <jaychong1203@gmail.com>
2024-05-03 19:05:32 -0700

    beautify

--- source/setup/problem_setup.py
+++ source/setup/problem_setup.py
@@ -2,11 +2,13 @@ import numpy as np
 from parameters import *
 from radmc3dPy.natconst import *
 
+# Load data
 vp_data = "wind_output.csv"
 vp = np.loadtxt(vp_data, delimiter=",", skiprows=1).reshape(nx, ny, nz)
 rhogas_data = "wind_density_output.csv"
 rhogas = np.loadtxt(rhogas_data, delimiter=",", skiprows=1).reshape(nx, ny, nz)
 
+# Meshgrid
 X, Y, Z = np.meshgrid(xc, yc, zc)
 R_plane = np.sqrt(X**2 + Y**2)
 r = R_plane
@@ -14,32 +16,30 @@ tan_y_x = np.arctan2(Y, X)
 tan_z_r = np.arctan2(Z, r)
 sqrt_GMstar_r = np.sqrt(GM_star / (r + 1e-10))
 
+# Velocity calculations
 Vx = (sqrt_GMstar_r * np.sin(tan_y_x) + vp * np.cos(tan_z_r) * np.cos(tan_y_x))
 Vy = (-sqrt_GMstar_r * np.cos(tan_y_x) + vp * np.cos(tan_z_r) * np.sin(tan_y_x))
 Vz = vp * np.sin(tan_z_r)
 
+# Other calculations
 rhod = rhogas * dusttogas
 tgas = np.full((nx, ny, nz), temp0)
 vturb = 792 * np.sqrt(tgas)  
 
-#-------------MAIN MODEL SETUP DONE----------------
-#-------------EMITTING SPECIES----------------
-#
+# EMITTING SPECIES
 # Write the molecule number density file. 
-#
 abunco = 1e-4
 factco = abunco/(2.3*mp)
-nco    = rhogas*factco
+nco = rhogas*factco
+
 with open('numberdens_co.inp','w+') as f:
     f.write('1\n')                       # Format number
     f.write('%d\n'%(nx*ny*nz))           # Nr of cells
     data = nco.ravel(order='F')          # Create a 1-D view, fortran-style indexing
     data.tofile(f, sep='\n', format="%13.6e")
     f.write('\n')
-#-------------EMITTING SPECIES----------------
-#
+
 # Write the grid file
-#
 with open('amr_grid.inp','w+') as f:
     f.write('1\n')                       # iformat
     f.write('0\n')                       # AMR grid style  (0=regular grid, no AMR)
@@ -53,9 +53,8 @@ with open('amr_grid.inp','w+') as f:
         f.write('%13.6e\n'%(value))      # Y coordinates (cell walls)
     for value in zi:
         f.write('%13.6e\n'%(value))      # Z coordinates (cell walls)
-#
+
 # Write the dust density file. Here we use a dust-to-gas ratio of 0.01
-#
 expected_entries = nx * ny * nz
 actual_entries = len(rhod.ravel(order='F'))
 
@@ -69,8 +68,6 @@ with open('dust_density.inp', 'w+') as f:
         f.write(f"{value:e}\n")
 
 # Write the gas velocity field
-#
-
 with open('gas_velocity.inp','w+') as f:
     f.write('1\n')
     f.write('%d\n' % (nx * ny * nz))
@@ -79,36 +76,29 @@ with open('gas_velocity.inp','w+') as f:
             for iz in range(nz):
                 f.write('%13.6e %13.6e %13.6e\n' % (Vx[ix, iy, iz], Vy[ix, iy, iz], Vz[ix, iy, iz]))
 
-
-#
 # Write the microturbulence file
-#
 with open('microturbulence.inp','w+') as f:
     f.write('1\n')                       # Format number
     f.write('%d\n'%(nx*ny*nz))           # Nr of cells
-    data = vturb.ravel(order='F')          # Create a 1-D view, fortran-style indexing
+    data = vturb.ravel(order='F')        # Create a 1-D view, fortran-style indexing
     data.tofile(f, sep='\n', format="%13.6e")
     f.write('\n')
-#
+
 # Write the gas temperature
-#
 with open('gas_temperature.inp','w+') as f:
     f.write('1\n')                       # Format number
     f.write('%d\n'%(nx*ny*nz))           # Nr of cells
-    data = tgas.ravel(order='F')          # Create a 1-D view, fortran-style indexing
+    data = tgas.ravel(order='F')         # Create a 1-D view, fortran-style indexing
     data.tofile(f, sep='\n', format="%13.6e")
     f.write('\n')
-#
+
 # Write the wavelength file
-#
 with open('wavelength_micron.inp','w+') as f:
     f.write('%d\n'%(nlam))
     for value in lam:
         f.write('%13.6e\n'%(value))
-#
-#
+
 # Write the stars.inp file
-#
 with open('stars.inp','w+') as f:
     f.write('2\n')
     f.write('1 %d\n\n'%(nlam))
@@ -116,9 +106,8 @@ with open('stars.inp','w+') as f:
     for value in lam:
         f.write('%13.6e\n'%(value))
     f.write('\n%13.6e\n'%(-tstar))
-#
+
 # Dust opacity control file
-#
 with open('dustopac.inp','w+') as f:
     f.write('2               Format number of this file\n')
     f.write('1               Nr of dust species\n')
@@ -127,16 +116,14 @@ with open('dustopac.inp','w+') as f:
     f.write('0               0=Thermal grain\n')
     f.write('silicate        Extension of name of dustkappa_***.inp file\n')
     f.write('----------------------------------------------------------------------------\n')
-#
+
 # Write the lines.inp control file
-#
 with open('lines.inp','w') as f:
     f.write('1\n')
     f.write('1\n')
     f.write('co    leiden    0    0\n')
-#
+
 # Write the radmc3d.inp control file
-#
 with open('radmc3d.inp','w+') as f:
     f.write('nphot = %d\n'%(nphot))
     f.write('scattering_mode_max = 0\n')   # Put this to 1 for isotropic scattering
