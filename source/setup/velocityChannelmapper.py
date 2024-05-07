3cf44d322a7fcd6c8689b9c1e61131d939da7aac
Jay <jaychong1203@gmail.com>
2024-05-06 22:51:28 -0700

    negative sorting mapper, so we can get a grid

--- source/setup/velocityChannelmapper.py
+++ source/setup/velocityChannelmapper.py
@@ -6,10 +6,10 @@ import os
 matplotlib.use('Agg')
 
 image_dir = './velocity_channels'
-image_files = sorted(os.listdir(image_dir))
+image_files = sorted(os.listdir(image_dir), key=lambda x: int(x.split('_')[1].split('.')[0]))
 
-plt.rcParams["figure.figsize"] = (15, 6)
-plots_per_row = 4
+plt.rcParams["figure.figsize"] = (20, 10)
+plots_per_row = 5
 num_slices = len(image_files)
 num_rows = (num_slices + plots_per_row - 1) // plots_per_row
 fig, axes = plt.subplots(num_rows, plots_per_row, figsize=(20, 5 * num_rows))
@@ -20,7 +20,8 @@ for index, file_name in enumerate(image_files):
     col = index % plots_per_row
     ax = axes[row, col]
     im = ax.imshow(a.image, origin='lower', cmap='hot')
-    ax.set_title(f'Velocity = {index+1} km/s')
+    velocity = file_name.split('_')[1].split('.')[0]
+    ax.set_title(f'Velocity = {velocity} km/s')
     ax.axis('off')
 
 fig.subplots_adjust(right=0.8)
