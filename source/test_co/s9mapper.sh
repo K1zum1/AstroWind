e845a544d12ebdfb768665a2c164b0fbeee51f6d
astro-sophie <sophie.scc.2@gmail.com>
2024-07-10 12:40:11 -0700

    troubleshooting p2

--- source/test_co/s9mapper.sh
+++ source/test_co/s9mapper.sh
@@ -1,5 +1,5 @@
 python3 master.py
 python3 problem_setup.py
-radmc3d mctherm setthreads 4
+radmc3d mctherm setthreads 10
 radmc3d image lambda 1301.3 incl 70 phi 0 zoomau -260 260 -260 260
 python3 generateimage.py
