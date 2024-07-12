75c4f049119a0516b972543e26f83234285904d1
reu24 <sophie.scc.2@gmail.com>
2024-07-12 14:57:33 -0700

    Small mods

--- source/test_co/generateimage.py
+++ source/test_co/generateimage.py
@@ -154,12 +154,8 @@ plt.ioff()
 
 img = readImage() 
 
-im_nx, im_ny = img.nx, img.ny # number of pixels on each axis
-pixsize_x, pixsize_y = img.sizepix_x, img.sizepix_y # pixel sizes in cm (radians?)
-dA = (pixsize_x * pixsize_y)/((distance*3.086e18)**2) # size of each pixel in steradians
-
 image_data = img.image.flatten()
-total_flux = np.sum(image_data) #*dA*1e23 #summed intensity converted to flux in Jy    
+total_flux = np.sum(image_data)
     
 result = plotImage2(img, flux=total_flux, log=True, maxlog=15, cmap=cm.hot, bunit='snu', dpc=distance, arcsec=True)
 
